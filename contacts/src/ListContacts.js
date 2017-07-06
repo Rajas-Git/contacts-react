4b3dd4c38ed7d16b07d17b1e19337c571a74e7c0
Rajus Dhoble <radhoble@microsoft.com>
2017-07-06 10:22:38 -0700

    Add delete functionality

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'; 
+import React from 'react'; 
 import PropTypes from 'prop-types';
 
-class ListContacts extends Component {
-    render() {
-        return (
+function ListContacts (props) {
+    return (
             <ol className='contact-list'> 
-                {this.props.contacts.map( (contact) => 
+                {props.contacts.map( (contact) => 
                     <li key={contact.id} className='contact-list-item'>
                         
                         <div className='contact-avatar' style={{
@@ -15,7 +14,9 @@ class ListContacts extends Component {
                             <p> {contact.name} </p>
                             <p> {contact.email}</p>
                         </div>
-                        <button className='contact-remove'>
+                        <button 
+                            onClick={() => props.onDeleteContact(contact)} 
+                            className='contact-remove'>
                             Remove
                         </button>
 
@@ -23,12 +24,37 @@ class ListContacts extends Component {
                 )}
             </ol>
         );
-    }
-
 }
 
+// class ListContacts extends Component {
+//     render() {
+//         return (
+//             <ol className='contact-list'> 
+//                 {this.props.contacts.map( (contact) => 
+//                     <li key={contact.id} className='contact-list-item'>
+                        
+//                         <div className='contact-avatar' style={{
+//                             backgroundImage: `url(${contact.avatarURL})`
+//                         }} />
+//                         <div className='contact-details'>
+//                             <p> {contact.name} </p>
+//                             <p> {contact.email}</p>
+//                         </div>
+//                         <button className='contact-remove'>
+//                             Remove
+//                         </button>
+
+//                     </li>   
+//                 )}
+//             </ol>
+//         );
+//     }
+
+// }
+
 ListContacts.propTypes = {
-    contacts: PropTypes.array.isRequired
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
 };
 
 export default ListContacts;
